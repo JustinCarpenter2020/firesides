ca32eda043254a96395fcd06af77849e53b05db6
TimDohmen <dohmentim@yahoo.com>
2021-02-05 11:50:28 -0700

    switch

--- w2/js-prac/app.js
+++ w2/js-prac/app.js
@@ -1,5 +1,8 @@
 // loop that goes backwards / decrements / increments by more than one (just to show it isnt always ++)
 
+  //forloop that counts from 100 - 0 
+
+
   for(let i = 100; i > 0; i--){
     console.log(i)
   }
@@ -7,11 +10,8 @@
 // iterate with a variable as your condition
 
   function varLoop(start, condition){
-    for(let i = start; i < condition; i++){
-      console.log(i)
-    }
+  //write a forloops using these params
   }
-
   varLoop(25, 50)
 
 // for vs for each vs for in (when to use for in and when not to)
@@ -54,9 +54,13 @@ people.forEach(p => {
   }
 })
 
+//NOTE 
+//only use a for in loop if you are iterating over an object
+//for in on an array causes it to be looped over as an object which is ultimately
+//slower than iterating by index on an array.
+//it also is not guaranteed to return the properties in sequential order as you might expect
 
 // ternary vs if
-
 people.forEach(p => { p.role ==  "TA" ? console.log(`Hello ${p.role} ${p.name}`) : p.role == 'Admin' ?console.log(`Hello ${p.role} ${p.name}, Have a nice day`) : console.log(`${p.name} is probably an instructor`);})
 
 
@@ -70,10 +74,61 @@ for(let i = 0; i < 10; i++){
     console.log(true)
   }
 
-
+  //no need to do if statement if youre just doing true/false 
   console.log(i%2 == 0)
 }
 
 
-
 //loop with a break and continue
+for(let i =0; i<25; i++){
+  console.log(i)
+  if(i != 20){
+    continue;
+  }else {
+    break;
+  }
+//continue tells your loop to keep looping while break stops your loop even if the
+//loop condition has not been met yet
+}
+
+
+let basket = [
+{
+  name: "Apple",
+  type: 'Fruit'
+},
+{
+  name: "Carrot",
+  type: 'Vegetable'
+},
+{
+  name: "Banana",
+  type: 'Fruit'
+},
+{
+  name: "Lettuce",
+  type: 'Vegetable'
+},
+{
+  name: "Steak",
+  type: 'Meat'
+},
+{
+  name: "D$ Special",
+  type: 'Other'
+}
+]
+
+basket.forEach(item=>{
+switch(item.type){
+  case 'Vegetable':
+    console.log('this a vege');
+    break;
+  case 'Fruit':
+    console.log('this a froot');
+    break;
+  default:
+    console.log("this not froot or vege");
+    break;
+}
+})
